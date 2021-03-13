993e1c72b8145132c6cf01658d7f46ad580d822e
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-13 16:51:20 -0500

    Update console.py

--- labtool/console.py
+++ labtool/console.py
@@ -7,7 +7,7 @@ def terminal ():
 
          while True:
             
-               user = input (>>> )
+               user = input (">>> ")
                prompt = user.split ()
 
                if not prompt:
@@ -16,10 +16,12 @@ def terminal ():
                       print (Fore.CYAN + "set ip => register the ip in the system")
                elif prompt[0] == 'set':
                       if prompt[1] == 'ip':
+                             global ip1
                              print (Fore.GREEN + "ip =>", prompt[2])
+                             ip1 = prompt[2]
                elif prompt[0] == 'run' or prompt[0] == 'exploit':
-                      if prompt[0] == 'set' and prompt[1] == 'ip':
-                             target = requests.get(url=prompt[2])
+                      if ip1 == ip1:
+                             target = requests.get(url=ip1)
                              header = dict(target.headers)
                              for x in header:
                                  print (x+ " : "+header[x])
