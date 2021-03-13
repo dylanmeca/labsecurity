a85e9555704c5407b9d14373b0a863b0e47021ca
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-13 16:32:49 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setuptools.setup(
+    name="lab_tool", 
+    version="1.0",
+    author="dylan14567",
+    author_email="",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dylan14567/lab_tool",
+    scripts = ['lab_tool'],
+    project_urls={
+        "Bug Tracker": "https://github.com/dylan14567/lab_tool/issues",
+    },
+    install_requires=[
+        "colorama",
+        "requests",
+        "wheel",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="server",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+)
