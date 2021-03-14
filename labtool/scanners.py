655acf8726fc108694ce604135e84716cbd93bfe
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-14 13:12:09 -0500

    Create scanners.py

---
+++ labtool/scanners.py
@@ -0,0 +1,13 @@
+import os, sys, time, nmap
+
+class scanner:
+
+        def ___init__(self):
+                #print ("Welcome")
+                return
+
+        def scanports(self,ip):
+                self.ip = ip
+                nm = nmap.PortScanner()
+                nm.scan(hosts=ip, arguments="--top-ports 1000 -sV --version-intensity 3")
+
