25275c5877bbad30dc528df07b0b84ee3bd0bb97
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-13 20:07:07 -0500

    Update console.py

--- labtool/console.py
+++ labtool/console.py
@@ -7,24 +7,40 @@ init(autoreset=True)
 def terminal ():
 
          while True:
-            
-               user = input ("lab-tool > ")
-               prompt = user.split ()
+               
+               try:
+                   user = input ("lab-tool > ")
+                   prompt = user.split ()
 
-               if not prompt:
-                      print (Fore.RED + "[*] ERROR")
-               elif prompt[0] == 'help':
-                      print (Fore.CYAN + "set ip => register the ip in the system")
-               elif prompt[0] == 'set':
-                      if prompt[1] == 'ip':
-                             global ip1
-                             print (Fore.GREEN + "ip =>", prompt[2])
-                             ip1 = prompt[2]
-               elif prompt[0] == 'exit' or prompt[0] == 'quit':
-                      break
-               elif prompt[0] == 'run' or prompt[0] == 'exploit':
-                      if ip1 == ip1:
+                   if not prompt:
+                         print (Fore.RED + "[*] ERROR")
+                   elif prompt[0] == 'help':
+                         print (Fore.CYAN + "set ip => register the ip in the system")
+                   elif prompt[0] == 'set':
+                         if prompt[1] == 'ip':
+                               global ip1
+                               print (Fore.GREEN + "ip =>", prompt[2])
+                               ip1 = prompt[2]
+                   elif prompt[0] == 'exit' or prompt[0] == 'quit':
+                         break
+                   elif prompt[0] == 'run' or prompt[0] == 'exploit':
+                         if ip1 == ip1:
                              header = header ()
                              header.headerweb (ip1)
-               else:
-                    print (Fore.RED + "[*] ERROR")
+                   else:
+                         print (Fore.RED + "[*] ERROR")
+               except ValueError:
+                        print (Fore.GREEN + "Error, there is a type of error.")
+                        sys.exit ()
+               except KeyboardInterrupt:
+                        print (Fore.GREEN + "Exiting...")
+                        sys.exit ()
+               except NameError:
+                        print (Fore.GREEN + "Error, the command I use is not available in the program code.")
+                        sys.exit ()
+               except SyntaxError:
+                        print (Fore.GREEN + "Error, the code has a syntax error.")
+                        sys.exit ()
+               except TypeError:
+                        print (Fore.GREEN + "Error, the code is misspelled.")
+                        sys.exit ()
