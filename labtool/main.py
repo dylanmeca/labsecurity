0151ff384ae4bad4e46c6b9c538f9212496c723d
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-13 20:47:32 -0500

    Create main.py

---
+++ labtool/main.py
@@ -0,0 +1,7 @@
+import sys, time, os
+
+class logo ():
+
+      def __init__(self):
+            os.system ("figlet lab_tool")
+            print ("Use the help command for more information about the commands")
