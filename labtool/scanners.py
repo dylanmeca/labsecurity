24a272315b176c80f414e77faab4c62d4f9e184b
agente45 <81337918+agente45@users.noreply.github.com>
2021-03-24 20:06:37 -0500

    Update scanners.py

--- labtool/scanners.py
+++ labtool/scanners.py
@@ -19,3 +19,17 @@ class scanner:
                              print (data + " : " + nm[ip]['tcp'][ports][data])
 
 
+        def scanport(self,ip,port):
+                self.ip = ip
+                self.port = port
+                nm = nmap.PortScanner()
+                nm.scan(ip, port, arguments='-sV --version-intensity 3')
+                print ("Command executed: {}".format(nm.command_line()))
+                print ("Protocols used: {}".format(nm[ip].all_protocols()))
+                print ("Machine status: {}".format(nm[ip].state()))
+
+                for ports in nm[ip]['tcp'].keys ():
+                     for data in nm[ip]['tcp'][ports]:
+                             print (data + " : " + nm[ip]['tcp'][ports][data])
+
+
