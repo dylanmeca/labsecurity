5483924082fc90cda0270e51f5c3adf75f27346c
dylan14567 <mundodylan14567dylanmotta78@gmail.com>
2021-04-15 13:37:00 -0500

    Update

--- labtool/main.py
+++ labtool/main.py
@@ -8,13 +8,13 @@ class main ():
              return
 
       def help (self):
-             print (Fore.CYAN + "set ip => register the ip in the system")
-             print (Fore.CYAN + "use headerweb => Use a script that extracts information from a website")
-             print (Fore.CYAN + "run or exploit => The run and exploit command is to start the system")
-             print (Fore.CYAN + "exit or quit => Is to exit the system ")
-             print (Fore.CYAN + "use scanports => Scan all the ports of an IP")
-             print (Fore.CYAN + "use scanport => Scan the port of an ip")
-             print (Fore.CYAN + "set port => register the port in the system")
-             print (Fore.CYAN + "use scanip => Geolocate an ip")
+             print (Fore.GREEN + "set ip => register the ip in the system")
+             print (Fore.GREEN + "use scanweb => Use a script that extracts information from a website")
+             print (Fore.GREEN + "run or exploit => The run and exploit command is to start the system")
+             print (Fore.GREEN + "exit or quit => Is to exit the system ")
+             print (Fore.GREEN + "use scanports => Scan all the ports of an IP")
+             print (Fore.GREEN + "use scanport => Scan the port of an ip")
+             print (Fore.GREEN + "set port => register the port in the system")
+             print (Fore.GREEN + "use scanip => Geolocate an ip")
                   
              
