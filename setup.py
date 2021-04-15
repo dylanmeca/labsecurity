3825a086ba1e091a0efe8dbdc462439263a9ec18
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-15 15:24:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="lab_tool", 
-    version="1.5",
+    version="1.6",
     author="dylan14567",
     author_email="",
     description="lab_tool is a system that allows you to do ethical hacking tests",
