285a22c43fa9f8613f4cf68143eb855bc2dcd74f
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-05-06 13:27:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 clear
 echo -e "\e[36mInstalling lab_tool....\e[0m"
+git clone https://github.com/dylan14567/lab_tool
+cd lab_tool
 chmod +x *
 python3 setup.py install
 cd
 rm -rf lab_tool
+echo -e "\e[36mComplete installation use the lab tool command to start the framework\e[0m"
