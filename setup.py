69a5b2c80cb93f77a2da06a7a3993c3c83916a76
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-05-10 19:02:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as f:
     README = f.read()
 
 setuptools.setup(
-    name="lab_tool", 
+    name="labsecurity", 
     version="1.6",
     author="dylan14567",
     author_email="",
-    description="lab_tool is a system that allows you to do ethical hacking tests",
+    description="labsecurity is a framework and its use is for ethical hacking and computer security",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/dylan14567/lab_tool",
-    scripts = ['lab_tool'],
+    url="https://github.com/dylan14567/labsecurity",
+    scripts = ['labsecurity'],
     project_urls={
-        "Bug Tracker": "https://github.com/dylan14567/lab_tool/issues",
+        "Bug Tracker": "https://github.com/dylan14567/labsecurity/issues",
         "Documentation": "https://dylan14567.github.io/2021/04/07/labtool-documentation.html",
     },
     install_requires=[
