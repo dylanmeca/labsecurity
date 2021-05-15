a43c935be0bc8ca0b6b8ba2819260c1fa3e615eb
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-05-15 17:00:25 -0500

    Create interpreter.py

---
+++ libsecurity/interpreter.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import time
+from .main import *
+from .scanners import *
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+class process:
+
+      def __init__(self):
+            return
+
+      def command_help (self, data):
+            self.data = data
+            main.help (self.data)
+
+      def command_set (self, data):
+            #Here is the part where the selections are put
+            self.data = data
+            self.prompt = data.split ()
+            
+            if self.prompt[1] == 'ip':
+                 print (Style.BRIGHT + Fore.GREEN + "ip =>", self.prompt[2])
+                 ip = self.prompt[2]
+                 self.ip = ip
+            elif self.prompt[1] == 'port':
+                 print (Style.BRIGHT + Fore.GREEN + "port =>", self.prompt[2])
+                 port = self.prompt[2]
+                 self.port = port
+            else:
+                 print (Style.BRIGHT + Fore.RED + "[*] Error, the command is misspelled")
+
+      def command_use (self, data):
+            #Here is the part where one of the following categories is used
+            self.data = data
+            self.prompt = data.split ()
+            if self.prompt[1] == 'scanweb':
+               print (Style.BRIGHT + Fore.GREEN + "use =>", self.prompt[1])
+               self.use = self.prompt[1]
+            elif self.prompt[1] == 'scanports':
+               print (Style.BRIGHT + Fore.GREEN + "use =>", self.prompt[1])
+               self.use = self.prompt[1]
+            elif self.prompt[1] == 'scanport':
+               print (Style.BRIGHT + Fore.GREEN + "use =>", self.prompt[1])
+               self.use = prompt[1]
+            elif self.prompt[1] == 'scanip':
+               print (Style.BRIGHT + Fore.GREEN + "use =>", self.prompt[1])
+               self.use = self.prompt[1]
+            else:
+                print (Style.BRIGHT + Fore.RED + "[*] Error, the command is misspelled")
+
+      def command_clear (self, data):
+            self.data = data
+            os.system (data)
+
+      def command_exit (self):
+            sys.exit ()
+
+      def command_run (self, data):
+
+
+                 
+                               
