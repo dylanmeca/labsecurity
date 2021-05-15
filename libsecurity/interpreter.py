e6644e369ffc061fb02a9ec46da0865ab69c34df
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-05-15 17:46:34 -0500

    Update interpreter.py

--- libsecurity/interpreter.py
+++ libsecurity/interpreter.py
@@ -74,7 +74,49 @@ class process:
             else:
                 print (Style.BRIGHT + Fore.RED + "[*] Error, the command is misspelled")
 
+      def terminal (self):
+            while True:
+               
+               try:
+                   user = input ("labsecurity > ")
+                   prompt = user.split ()
+                   
+                   if prompt[0] == 'help':
+                        process.command_help (user)
+                   elif prompt[0] == 'set':
+                        process.command_set (user)
+                   elif prompt[0] == 'use':
+                        process.command_use (user)
+                   elif prompt[0] == 'clear':
+                        process.command_clear (user)
+                   elif prompt[0] == 'exit':
+                        process.command_exit ()
+                        break
+                   elif prompt[0] == 'run' or prompt[0] == 'exploit': 
+                        process.command_run ('exit')
+                   else:
+                         print (Style.BRIGHT + Fore.RED + "[*] ERROR, use the help command for more information")
 
+               except ValueError:
+                        print (Style.BRIGHT + Fore.RED + "[*] Error, there is a type of error.")
+                        sys.exit ()
+               except KeyboardInterrupt:
+                        print (Style.BRIGHT + Fore.BLUE + "[*] Exiting...")
+                        sys.exit ()
+               except NameError:
+                        print (Style.BRIGHT + Fore.RED + "[*] Error, the command I use is not defined in the program code.")
+                        sys.exit ()
+               except SyntaxError:
+                        print (Style.BRIGHT + Fore.RED + "[*] Error, the code has a syntax error.")
+                        sys.exit ()
+               except TypeError:
+                        print (Style.BRIGHT + Fore.RED + "[*] Error, the code is misspelled.")
+                        sys.exit ()
+               except IndexError:
+                        print (Style.BRIGHT + Fore.RED + "[*] Error, the command you used is incomplete.")
+                        sys.exit ()
+               
+    
 
                  
                                
