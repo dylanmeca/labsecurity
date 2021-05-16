bb70c28c6c2af5f6ff135fcb35cb47512f7e2b31
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-05-16 15:40:23 -0500

    Update main.py

--- libsecurity/main.py
+++ libsecurity/main.py
@@ -2,13 +2,13 @@ from colorama import init, Fore, Style
 
 init(autoreset=True)
 
-class main ():
+class interpreter:
 
       def __init__(self):
              #print ("Hello World")
              return
 
-      def help (self):
+      def command_help (self):
              print (Style.BRIGHT + Fore.GREEN + "set ip => register the ip in the system")
              print (Style.BRIGHT + Fore.GREEN + "use scanweb => Use a script that extracts information from a website")
              print (Style.BRIGHT + Fore.GREEN + "run or exploit => The run and exploit command is to start the system")
