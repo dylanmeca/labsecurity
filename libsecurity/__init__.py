3eba9a9aeecb0c69aafa002bc8978a3954b9588d
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-07-06 08:45:24 -0500

    Update __init__.py

--- libsecurity/__init__.py
+++ libsecurity/__init__.py
@@ -1,1 +1,13 @@
+from libsecurity.scanners import *
+from libsecurity.main import *
 
+scanner = scanner()
+interpreter = interpreter()
+
+scanports = scanner.scanports
+scanport = scanner.scanport
+scanip = scanner.scanip
+scanweb = scanner.scanweb
+
+help = interpreter.help
+command_ls = interpreter.command_ls
